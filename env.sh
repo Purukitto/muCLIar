f824c321df8eedfbacbae610c64a09580f3748f6
Pulkit Sambhavi Singh <49859368+Purukitto@users.noreply.github.com>
2020-10-11 14:43:49 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -6,24 +6,6 @@ eval "$(conda shell.bash hook)"
 conda activate test
 pip install -r requirements.txt
 sudo apt-get update -y
-sudo apt-get install -y xvfb
-
-#get chromedriver
-RES="$(google-chrome --version)"
-RES="$(echo $RES | cut -d' ' -f3)"
-A="$(echo $RES | cut -d'.' -f1)"
-if [[ $A == "79" ]];
-then
-    echo "Getting chrome driver for version 79 ..."
-    sudo wget -P /usr/local/bin/ https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
-    sudo unzip /usr/local/bin/chromedriver_linux64.zip -d /usr/local/bin
-fi
-if [[ $A == "80" ]]
-then 
-    echo "Getting chrome driver for version 80 ..."
-    sudo wget -P /usr/local/bin https://chromedriver.storage.googleapis.com/80.0.3987.16/chromedriver_linux64.zip
-    sudo unzip /usr/local/bin/chromedriver_linux64.zip -d /usr/local/bin
-fi    
 
 #move into local/bin
 SHELL_TARGDIR="/usr/local/bin"
